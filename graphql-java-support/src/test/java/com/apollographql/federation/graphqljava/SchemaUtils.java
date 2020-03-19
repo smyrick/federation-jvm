b9a27d26ff647138d5b6cf89e489935cb91889f2
Sachin D. Shinde <sachin@apollographql.com>
2020-03-18 22:51:27 -0700

    Make SchemaUtils.standardDirectives private

--- graphql-java-support/src/test/java/com/apollographql/federation/graphqljava/SchemaUtils.java
+++ graphql-java-support/src/test/java/com/apollographql/federation/graphqljava/SchemaUtils.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 final class SchemaUtils {
-    static final Set<String> standardDirectives =
+    private static final Set<String> standardDirectives =
             new HashSet<>(Arrays.asList("deprecated", "include", "skip"));
 
     private SchemaUtils() {
